6d72ac219eb3140d0ca90f3eaaec41cf0ce5ffa0
Shahriar220 <shahriar.uchchash@gmail.com>
2020-10-15 23:05:22 +0600

    mod

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hi');
-    console.log(req)
+    res.send('Hi buddy');
+
 })
 
 const PORT = process.env.PORT || 3000;
