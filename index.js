ec1c6124a582bee101ada0700142d3197cc44d8f
Shahriar220 <shahriar.uchchash@gmail.com>
2020-10-20 21:53:05 +0600

    logout from app added with user view

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const MongoClient = require('mongodb').MongoClient;
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+
+mongoose.connect('mongodb://localhost:27017/emaily-dev', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('ok'));
+
+// const client = new MongoClient(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//     const collection = client.db("emaily-dev").collection("users");
+//     // perform actions on the collection object
+//     console.log('Connected')
+//     client.close();
+
+// });
+
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hi buddy');
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+}))
 
-})
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`listening to ${PORT}`));
+
+//app installation //mongodb+srv://shahriar:<password>@api.9qfo6.mongodb.net/<dbname>?retryWrites=true&w=majority
+//mongo shell mongo "mongodb+srv://api.9qfo6.mongodb.net/<dbname>" --username shahriar
